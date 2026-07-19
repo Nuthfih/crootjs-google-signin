b17627abb1e197b7cd5a646de5ea60df6561a8e3
Nuthfih <asatriaraghib@gmail.com>
2026-07-19 13:00:37 +0700

    adding logout

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ import { postJSON } from "https://cdn.jsdelivr.net/gh/crootjs/lib@0.0.1/api.js";
 
 const urlBackend = "https://tugas-google-nuthfih.sao.dom.my.id/api/verify"; 
 const divStatus = document.getElementById("status");
+const loginArea = document.getElementById("login-area");
+const userArea = document.getElementById("user-area");
+const welcomeText = document.getElementById("welcome-text");
+const logoutBtn = document.getElementById("logout-btn");
 
 function responseData(hasil) {
     divStatus.style.display = "block";
@@ -9,18 +13,29 @@ function responseData(hasil) {
     if(hasil.status === 200) {
         divStatus.className = "success";
         divStatus.innerHTML = `<strong>Berhasil!</strong> ${hasil.data.message}`;
-        console.log("Data user tervalidasi:", hasil.data);
+        
+        loginArea.style.display = "none";
+        
+        userArea.style.display = "block";
+        welcomeText.innerText = `Selamat datang! ID Unikmu: ${hasil.data.user_id}`;
+        
+        console.log("Data user tervalidasi dari Golang:", hasil.data);
     } else {
         divStatus.innerHTML = "Gagal memverifikasi di sisi server.";
+        console.error("Respon server:", hasil);
     }
 }
 
 window.tanganiKredensialGoogle = (response) => {
     console.log("Token JWT dari Google diterima!");
-    
-    const payload = {
-        token: response.credential
-    };
-
+    const payload = { token: response.credential };
     postJSON(urlBackend, payload, responseData);
 };
+
+logoutBtn.addEventListener("click", () => {
+    userArea.style.display = "none";
+    divStatus.style.display = "none";
+    loginArea.style.display = "block";
+    
+    console.log("User telah logout.");
+});
